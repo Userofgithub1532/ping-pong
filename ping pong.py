616d3a8a46d5b7645bf6ec055e1e3d2e69ec461c
Userofgithub1532 <129618160+Userofgithub1532@users.noreply.github.com>
2023-09-16 18:18:24 +0300

    Update ping pong.py

--- ping pong.py
+++ ping pong.py
@@ -8,7 +8,8 @@ class player():
         self.h = h
         self.img = img
         self.img_new = pygame.transform.scale(pygame.image.load(self.img), (self.w,self.h))
-        self.img_rect = self.img_new.get_rect()
+        self.img_rect = self.img_new.get_rect()(center = (self.x, self.y))
+player = Player(50,190,30,175,'platforma.png')
 W,H = (700,450)
 name = pygame.display.set_caption("ping_pong")
 scr = pygame.display.set_mode((W,H))
@@ -19,6 +20,7 @@ while run:
         if event.type == pygame.QUIT:
             run = False
     scr.blit(bg, (0,0))
+    scr.blit(player.img_new, (player.img_rect.x,player.img_rect.y))
     pygame.display.update()
 
 
